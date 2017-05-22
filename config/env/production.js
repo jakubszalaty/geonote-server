b0d87f65c00c9d89c9cc759fd07061c3e83b0869
Kuba Szalaty <jakub.szalaty@gmail.com>
2017-05-22 13:38:45 +0200

    Azure config

--- config/env/production.js
+++ config/env/production.js
@@ -92,7 +92,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   blueprints: {
-    shortcuts: false,
+    shortcuts: true,
   },
 
   /***************************************************************************
@@ -350,6 +350,11 @@ module.exports = {
     //--------------------------------------------------------------------------
 
   },
+  // Use process.env.port to handle web requests to the default HTTP port
+  port: process.env.port,
+  // Increase hooks timout to 30 seconds
+  // This avoids the Sails.js error documented at https://github.com/balderdashy/sails/issues/2691
+  hookTimeout: 30000,
 
 
 
